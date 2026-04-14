967ea043afa9c761cbf8778cb68a6e3445126c0d
Sujal Patel <sujal7455@gmail.com>
2026-04-14 16:27:53 +0530

    new patterns

--- 01_pattern.ts
+++
@@ -1,9 +0,0 @@
-
-// Pattern Programming
-
-for (let i:number = 1; i <= 5; i++) {
-  for (let j:number = 1; j <= i; j++) {
-    process.stdout.write("*");
-  }
-  process.stdout.write("\n");
-}
---
+++ 01_patterns.js
@@ -0,0 +1,7 @@
+import PromptSync from "prompt-sync";
+const prompt = PromptSync();
+
+function clog(arg) {
+  process.stdout.write(arg);
+}
+
